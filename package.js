b45714d37b3b58463dc50b9125a3b0cab63b09c0
Bruno Orcha García <gimcoo@gmail.com>
2015-01-12 21:07:47 +0100

    Adding service-configuration dependency

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'gimco:accounts-ui-latch',
   summary: 'Latch integration for accounts-ui package',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/gimco/meteor-accounts-ui-latch'
 });
 
